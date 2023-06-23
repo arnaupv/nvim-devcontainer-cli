63cb60e780d17a46821abc091cbe490d8aca2198
Arnau Pont Vilchez <47103654+arnaupv@users.noreply.github.com>
2023-06-23 11:14:46 +0200

    Adding unit test and executing them using a makefile command (the plenary.busted lua package is used for running the unit tests)

---
+++ tests/devcontainer_cli/devcontainer_cli_spec.lua
@@ -0,0 +1,9 @@
+describe("setup", function()
+	it("works with default", function()
+		assert("param", "param")
+	end)
+
+	it("works with custom var", function()
+		assert("param", "param")
+	end)
+end)
