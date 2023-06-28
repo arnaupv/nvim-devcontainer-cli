3f95c2b6d466bfddb52c9912221b285ded911f68
Arnau Pont Vilchez <47103654+arnaupv@users.noreply.github.com>
2023-06-29 01:16:17 +0200

    Installing dotfiles and oh-my-zsh in case they do not exist already in the docker container

--- bin/devcontainer_setup_scripts/none_root_setup.sh
+++ bin/devcontainer_setup_scripts/none_root_setup.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 set -xe
 
-# TODO: Installing my dotfiles. Instead of having this part of the code hardcoded, this config param has to be as a plugin config param.
-MY_HOME=$HOME # /root/
-git clone https://github.com/arnaupv/dotfiles.git ${MY_HOME}/dotfiles
-cd ${MY_HOME}/dotfiles
-./install.sh
-cd -
+MY_HOME=${HOME} # /root/
 
-# TODO: By default the zsh shell is installed inside the dev container. However this is not ideal, as it should be a pluging config param.
-sh -c "$(curl -L https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)"
+# TODO: Installing my dotfiles. Instead of having this part of the code hardcoded, this config param has to be as a plugin config param.
+# If dotfiles folder does not exist in MY_HOME directory, clone the repo and run install.sh script.
+if [ ! -d "${MY_HOME}"/dotfiles ]; then
+	git clone https://github.com/arnaupv/dotfiles.git "${MY_HOME}"/dotfiles
+	cd "${MY_HOME}"/dotfiles
+	./install.sh
+	cd -
+fi
 
 # TODO: such linters could be automatically installed by Neovim Plugins (Mason).
 # python packages: formating and linting
 pip install black isort
+# TODO: By default the zsh shell is installed inside the dev container. However this is not ideal, as the type of SHELL should be a plugin config param.
+if [ ! -d "${MY_HOME}"/.oh-my-zsh/ ]; then
+	sh -c "$(curl -L https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)"
+fi
