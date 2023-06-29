3f172ee2cd3451a45fdb3213620acac57c703ced
Arnau Pont Vilchez <47103654+arnaupv@users.noreply.github.com>
2023-06-29 15:16:49 +0200

    Fixing possible variable issues highlighted by the bash linter

--- bin/spawn_devcontainer.sh
+++ bin/spawn_devcontainer.sh
@@ -8,7 +8,7 @@ get_project_root_folder() {
 }
 
 workspace=$(get_project_root_folder)
-cd ${workspace}
+cd "${workspace}"
 
 remove_flag=""
 if [ "$1" = "true" ]; then
@@ -16,18 +16,18 @@ if [ "$1" = "true" ]; then
 	shift
 fi
 
-NVIM_DEVCONTAINER_CLI_FOLDER=$(echo ${HOME}/.local/share/nvim/lazy/nvim-devcontainer-cli/ | sed "s|^$HOME||")
+NVIM_DEVCONTAINER_CLI_FOLDER=$(echo "${HOME}"/.local/share/nvim/lazy/nvim-devcontainer-cli/ | sed "s|^$HOME||")
 HOME_IN_DOCKER_CONTAINER="/home/my-app/"
-NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER=${HOME_IN_DOCKER_CONTAINER}${NVIM_DEVCONTAINER_CLI_FOLDER}
+NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER=${HOME_IN_DOCKER_CONTAINER}"${NVIM_DEVCONTAINER_CLI_FOLDER}"
 DEVCONTAINER_OVERRIDE_CONFIG=.devcontainer/devcontainer-override.json
 
 devcontainer up $remove_flag \
 	--mount type=bind,source=${HOME}/.config/github-copilot,target=/home/my-app/.config/github-copilot \
-	--mount type=bind,source=${HOME}/${NVIM_DEVCONTAINER_CLI_FOLDER},target=${NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER} \
-	--workspace-folder ${workspace}
+	--mount type=bind,source="${HOME}"/"${NVIM_DEVCONTAINER_CLI_FOLDER}",target="${NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER}" \
+	--workspace-folder "${workspace}"
 
 # Setting Up Devcontainer ()
 # TODO: Instead of having 2 different scripts (for root and not root users) we should have a unique script (simplifying the usage of the plugin)
-devcontainer exec --override-config ${DEVCONTAINER_OVERRIDE_CONFIG} --workspace-folder ${workspace} sh ${NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER}/bin/devcontainer_setup_scripts/root_setup.sh
+devcontainer exec --override-config ${DEVCONTAINER_OVERRIDE_CONFIG} --workspace-folder "${workspace}" sh "${NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER}"/bin/devcontainer_setup_scripts/root_setup.sh
 # Setting Up Devcontainer (no root permits)
-devcontainer exec --workspace-folder ${workspace} sh ${NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER}/bin/devcontainer_setup_scripts/none_root_setup.sh
+devcontainer exec --workspace-folder "${workspace}" sh "${NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER}"/bin/devcontainer_setup_scripts/none_root_setup.sh
