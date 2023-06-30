25b3ee3db5ec41d046eac20d19ac9d32aca86a56
Arnau Pont Vilchez <47103654+arnaupv@users.noreply.github.com>
2023-06-30 18:33:47 +0200

    Fixing the Docker UI issue: circunventing the problem using docker exec instead of devcontainer exec ...

--- bin/open_shell_in_devcontainer.sh
+++ bin/open_shell_in_devcontainer.sh
@@ -4,7 +4,16 @@
 
 set -e
 
-WORKSPACE="./"
+WORKSPACE="$(pwd)"
 SHELL="zsh"
 
-devcontainer exec --workspace-folder ${WORKSPACE} ${SHELL}
+workspace_folder=$(devcontainer read-configuration --include-merged-configuration --log-format json --workspace-folder . 2>/dev/null | jq .workspace.workspaceFolder | sed 's/"//g')
+docker_id=$(docker ps -q -a --filter label=devcontainer.local_folder="${WORKSPACE}" --filter label=devcontainer.config_file="${WORKSPACE}"/.devcontainer/devcontainer.json)
+docker exec -it "${docker_id}" bash -c "cd ${workspace_folder} && ${SHELL}"
+# docker exec -it "${docker_id}" ${SHELL}
+
+# TODO: It would be great to use th devcontainer exec command as long as we know how to fix the visualization issue inside the docker container!
+# If you want to give a try, just comment the docker exect -it ... above and uncommend the line below. Then execute the :DevcontainerConnect and open vim.
+# There you will see visualzation issues which do not occur when connectint to docker via docker exec
+
+# devcontainer exec --workspace-folder ${WORKSPACE} ${SHELL}
