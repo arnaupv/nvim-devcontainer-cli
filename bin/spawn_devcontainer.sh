8b7d3cc22e6a40ecbfb2312211dc56b7449221ef
Arnau Pont <arnau.pont@paxport.net>
2024-04-11 12:38:51 +0200

    getting rid of env part, as there is no need to differentiate between "pro" and "dev"

--- bin/spawn_devcontainer.sh
+++ bin/spawn_devcontainer.sh
@@ -6,9 +6,6 @@ help() {
 	echo "Options:"
 	echo "  -r, --remove-existing-container"
 	echo "  Description: Remove existing devcontainer before creating a new one."
-	echo "  -e, --env [pro|dev]"
-	echo "  Description: Environment to setup. If 'dev' is specified, the plugin folder is mounted in the container (only needed when developing the plugin)"
-	echo "  Default: pro"
 	echo "  -d, --root_directory"
 	echo "  Description: root_dir where the .devcontainer folder is located"
 	echo "  -s, --setup-environment-repo"
@@ -27,13 +24,12 @@ help() {
 	echo "  Description: Show help."
 	echo ""
 	echo "Example:"
-	echo "  $0 -r -e dev -s https://www.github.com/username/setup-environment -i './install.sh -p nvim zsh stow' -D https://github.com/LazyVim/starter -I 'mv ~/dotfiles ~/.config/nvim'"
+	echo "  $0 -r -s https://www.github.com/username/setup-environment -i './install.sh -p nvim zsh stow' -D https://github.com/LazyVim/starter -I 'mv ~/dotfiles ~/.config/nvim'"
 	echo ""
 }
 
 # Default values
 remove_existing_container=""
-env="pro"
 
 # Handle command-line arguments
 while [ $# -gt 0 ]; do
@@ -41,17 +37,6 @@ while [ $# -gt 0 ]; do
 	-r | --remove-existing-container)
 		remove_existing_container="--remove-existing-container"
 		shift
-		;;
-	-e | --env)
-		if [ "$2" = "dev" ]; then
-			env="dev"
-		elif [ "$2" = "pro" ]; then
-			env="pro"
-		else
-			echo "Invalid env. Please specify either 'dev' or 'pro' for -e."
-			exit 1
-		fi
-		shift 2
 		;;
 	-d | --root_directory)
 		root_dir=$2
@@ -107,10 +92,6 @@ NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER=${REMOTE_HOME}"${NVIM_DEVCONTAI
 DOTFILES_DIR="${REMOTE_HOME}/$nvim_dotfiles_dir"
 SETUP_ENVIRONMENT_DIR="${REMOTE_HOME}/$setup_environment_dir"
 
-# Mount the plugin folder only if --env is set to dev:
-# if [ "$env" = "dev" ]; then
-# 	MOUNT_BIND_PLUGIN="--mount type=bind,source=${HOME}/${NVIM_DEVCONTAINER_CLI_FOLDER},target=${NVIM_DEVCONTAINER_CLI_FOLDER_IN_DOCKER_CONTAINER}"
-# fi
 devcontainer up ${remove_existing_container} \
 	--dotfiles-repository "${setup_environment_repo}" \
 	--dotfiles-target-path "${SETUP_ENVIRONMENT_DIR}" \
